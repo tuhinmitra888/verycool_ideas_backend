0bf8b932e474fd80f3d894eae64ab72bdc15d178
Tuhin Mitra <tuhinmitra@Tuhins-MacBook-Pro.local>
2022-01-16 14:37:58 +0100

    Remove redundant code

--- src/script.js
+++ src/script.js
@@ -2,19 +2,6 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-async function main() {
-  const newIdea = await prisma.idea.create({
-    data: {
-      type: 'Hobbies',
-      name: 'Wood pecking',
-      description: 'Kill time productively',
-    },
-  })
-  console.log(newIdea)
-  const allIdeas = await prisma.idea.findMany()
-  console.log(allIdeas)
-}
-
 main()
   .catch((e) => {
     throw e
